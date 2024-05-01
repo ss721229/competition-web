2fadca5b152cf2097c55e0bdca84ec28d59ef1bf
Sangmin <ss721229@naver.com>
2024-05-01 13:43:41 +0900

    feat: add scraping wevity, modify save functions to save wevity data

--- save.py
+++ save.py
@@ -7,11 +7,21 @@ from datetime import datetime
 from mainpage.models import Competition
 
 def save_data_to_database(platform, title, url, application_start, application_end):
-    for t, u, start, end in zip(title, url, application_start, application_end):
-        Competition.objects.create(
-            platform=platform,
-            title=t,
-            url=u,
-            application_start=datetime.strptime(start, "%y.%m.%d").strftime("%Y-%m-%d"),
-            application_end=datetime.strptime(end,"%y.%m.%d").strftime("%Y-%m-%d")
-        )
+    if platform == '링커리어':
+        for t, u, start, end in zip(title, url, application_start, application_end):
+            Competition.objects.create(
+                platform=platform,
+                title=t,
+                url=u,
+                application_start=datetime.strptime(start, "%y.%m.%d").strftime("%Y-%m-%d"),
+                application_end=datetime.strptime(end,"%y.%m.%d").strftime("%Y-%m-%d")
+            )
+    elif platform == '위비티':
+        for t, u, start, end in zip(title, url, application_start, application_end):
+            Competition.objects.create(
+                platform=platform,
+                title=t,
+                url=u,
+                application_start=datetime.strptime(start,'%Y-%m-%d'),
+                application_end=datetime.strptime(end,'%Y-%m-%d')
+            )
---
+++ scraping_wevity.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+from save import save_data_to_database
+
+def scraping_wevity():
+    page_number = 22
+    title, url, application_start, application_end = [], [], [], []
+
+    while page_number:
+        response = requests.get(f"https://www.wevity.com/?c=find&s=1&mode=ing&gp={page_number}")
+        soup = BeautifulSoup(response.text, 'html.parser')
+        element_tit = soup.find_all(class_="tit")[1:]
+
+        if len(element_tit) == 0:
+            print(f"모든 페이지 스크래핑 완료")
+            break
+        print(f"{page_number} 페이지 스크래핑 시작")
+
+        for e in element_tit:
+            text = ''
+            for element in e.find('a').contents:
+                if element.name == 'span':
+                    continue
+                text += str(element).strip()
+            title.append(text)
+            url.append('https://www.wevity.com/' + (e.find('a').get('href')))
+
+        for u in url:
+            time.sleep(0.3)
+            response = requests.get(u)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            element_dday_area = soup.find_all('li', class_="dday-area")
+            for e in element_dday_area:
+                text = ''
+                for element in e.contents:
+                    if element.name == 'span':
+                        continue
+                    text += str(element).strip()
+                application_start.append(text.split(' ')[0])
+                application_end.append(text.split(' ')[2])
+
+        time.sleep(0.3)
+        print(f"{page_number} 페이지 스크래핑 완료")
+        page_number += 1
+
+    return title, url, application_start, application_end
+
+
+if __name__ == "__main__":
+    title, url, application_start, application_end = scraping_wevity()
+    save_data_to_database('위비티', title, url, application_start, application_end)
